6f67722315ea519b50d45a11a3dc20094729e13b
Prashant288 <prashantsemwal311@gmail.com>
2022-11-22 14:02:06 +0530

    Update urls.py

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -22,6 +22,6 @@ admin.site.index_title = "Welcome to Admin Portal"
 
 
 urlpatterns = [
-    path('https://prashant288.github.io/portfolio/', include('polls.urls')),
+    path('/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
